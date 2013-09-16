db0813add38a20cc31e749af33723e20cf0f5d1a
ryalein <ryalein@googlemail.com>
2013-09-16 20:27:51 +0200

    initial commit

---
+++ common/net/monkeystudios/nc/NetherCraft.java
@@ -0,0 +1,14 @@
+package net.monkeystudios.nc;
+
+/**
+ * NetherCraft
+ *
+ * NetherCraft
+ *
+ * @author ryalein
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ *
+ */
+public class NetherCraft {
+
+}
